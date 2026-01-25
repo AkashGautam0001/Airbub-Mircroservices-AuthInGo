d2f8371dd4aae7c0a829ef2d814f8f416005c26d
Akash Gautam <akashgautam01584@gmail.com>
2026-01-25 21:10:51 +0530

    added db config

---
+++ config/db/db.go
@@ -0,0 +1,39 @@
+package config
+
+import (
+	env "AuthInGo/config/env"
+	"database/sql"
+	"fmt"
+
+	"github.com/go-sql-driver/mysql"
+)
+
+func SetupDB() (*sql.DB, error) {
+	cfg := mysql.NewConfig()
+	cfg.User = env.GetString("DB_USER", "root")
+	cfg.Passwd = env.GetString("DB_PASSWORD", "root")
+	cfg.Net = env.GetString("DB_NET", "tcp")
+	cfg.Addr = env.GetString("DB_ADDR", "localhost:3306")
+	cfg.DBName = env.GetString("DB_NAME", "auth_dev")
+
+	fmt.Println("Connecting to Database", cfg.DBName, cfg.FormatDSN())
+
+	db, err := sql.Open("mysql", cfg.FormatDSN())
+	if err != nil {
+		fmt.Println("Error connecting to DB", err)
+		return nil, err
+	}
+
+	fmt.Println("Trying to connect to database...")
+
+	pingErr := db.Ping()
+
+	if pingErr != nil {
+		fmt.Println("Error connecting to DB", err)
+		return nil, pingErr
+	}
+
+	fmt.Println("Connected to database successfully!", cfg.DBName)
+
+	return db, nil
+}
--- main.go
+++ main.go
@@ -2,12 +2,15 @@ package main
 
 import (
 	"AuthInGo/app"
+	dbConfig "AuthInGo/config/db"
 	config "AuthInGo/config/env"
 )
 
 func main() {
 	config.Load()
+
 	cfg := app.NewConfig()
 	app := app.NewApplication(cfg)
+	dbConfig.SetupDB()
 	app.Run()
 }
